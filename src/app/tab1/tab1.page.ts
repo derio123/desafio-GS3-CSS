b053db4c2e7f252d88dce96ec39330f91816f25d
Vinícius Gonçalves de Andrade <viniciusgandrade@gmail.com>
2022-02-13 23:03:49 -0300

    Initial commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+
+  saudacao = 'Bom dia';
+
+  limiteUtilizado = 12132.4;
+  proximasFaturas = 432.4;
+  limiteDisponivel = 7233.4;
+  total: number;
 
   constructor() {}
+
+  ngOnInit(): void {
+    this.total = this.limiteDisponivel + this.limiteUtilizado + this.proximasFaturas;
+  }
 
 }
